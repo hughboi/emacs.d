c9656442f1102a46a849ccda20016a9efac02e99
hughboi <hdc@ualberta.ca>
2018-01-15 15:05:41 -0700

    new theme

--- init.el
+++ init.el
@@ -1,14 +1,37 @@
+;; Set specific emacs variables
+(setq column-number-mode t)
+
 (add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(package-initialize)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (intellij)))
+ '(custom-enabled-themes (quote (github-modern)))
  '(custom-safe-themes
    (quote
-    ("e3fc83cdb5f9db0d0df205f5da89af76feda8c56d79a653a5d092c82c7447e02" default)))
+    ("696171d66b2f152e9d71755be171a6d9ac3be0b0d3deb7d8cbf2dd6132306638" "e3fc83cdb5f9db0d0df205f5da89af76feda8c56d79a653a5d092c82c7447e02" default)))
+ '(ensime-sem-high-faces
+   (quote
+    ((var :foreground "#000000" :underline
+	  (:style wave :color "yellow"))
+     (val :foreground "#000000")
+     (varField :foreground "#600e7a" :slant italic)
+     (valField :foreground "#600e7a" :slant italic)
+     (functionCall :foreground "#000000" :slant italic)
+     (implicitConversion :underline
+			 (:color "#c0c0c0"))
+     (implicitParams :underline
+		     (:color "#c0c0c0"))
+     (operator :foreground "#000080")
+     (param :foreground "#000000")
+     (class :foreground "#20999d")
+     (trait :foreground "#20999d" :slant italic)
+     (object :foreground "#5974ab" :slant italic)
+     (package :foreground "#000000")
+     (deprecated :strike-through "#000000"))))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
