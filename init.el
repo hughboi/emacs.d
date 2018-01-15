7bf200c29c0426631820b9970e3afbf5f87aa39a
hughboi <hdc@ualberta.ca>
2018-01-14 18:47:07 -0700

    emacs.d customization initial commit

---
+++ init.el
@@ -0,0 +1,19 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (intellij)))
+ '(custom-safe-themes
+   (quote
+    ("e3fc83cdb5f9db0d0df205f5da89af76feda8c56d79a653a5d092c82c7447e02" default)))
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
